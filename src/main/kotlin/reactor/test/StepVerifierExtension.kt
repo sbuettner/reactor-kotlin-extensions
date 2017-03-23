6d96ea1dc6f8bce5b98f78188ad01065310e5ef1
Sebastien Deleuze <sdeleuze@pivotal.io>
2017-03-23 17:43:40 +0100

    Add more StepVerifier extensions

--- src/main/kotlin/reactor/test/StepVerifierExtension.kt
+++ src/main/kotlin/reactor/test/StepVerifierExtension.kt
@@ -3,3 +3,11 @@ package reactor.test
 import kotlin.reflect.KClass
 
 fun StepVerifier.LastStep.expectError(kClass: KClass<out Throwable>) : StepVerifier = expectError(kClass.java)
+
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
+inline fun <reified T : Throwable> StepVerifier.LastStep.expectError() : StepVerifier = expectError(T::class.java)
+
+fun StepVerifier.LastStep.verifyError(kClass: KClass<out Throwable>) = verifyError(kClass.java)
+
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
+inline fun <reified T : Throwable> StepVerifier.LastStep.verifyError() = verifyError(T::class.java)
